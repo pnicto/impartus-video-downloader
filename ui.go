0579f4b550c3e77c6f44b934d74de31308cf33b5
pnicto <dharanikanth789@gmail.com>
2023-05-20 04:20:25 +0530

    feat: Add user interface for choosing lectures

--- ui.go
+++ ui.go
@@ -19,3 +19,22 @@ func ChooseCourse(courses Courses) int {
 
 	return choice - 1
 }
+
+func ChooseLectures(lectures Lectures) (int, int) {
+	var startIndex int
+	var endIndex int
+
+	fmt.Println("Choose a course to download from")
+	fmt.Println()
+	for i, lecture := range lectures {
+		fmt.Printf("%3d LEC %d  %s\n", i+1, lecture.SeqNo, lecture.Topic)
+	}
+	fmt.Println()
+
+	// TODO: Add better range examples here
+	fmt.Println("Enter a range")
+	// TODO: Check input is within range and a number
+	fmt.Scanf("%d %d", &startIndex, &endIndex)
+
+	return startIndex - 1, endIndex - 1
+}
