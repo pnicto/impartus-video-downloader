a3ef099220befde53d9e6ce614f5a4743a40037a
pnicto <dharanikanth789@gmail.com>
2023-05-20 17:12:52 +0530

    refactor: Return outfile path on joining chunks
    Perhaps I can use these paths to join two views

--- ffmpeg.go
+++ ffmpeg.go
@@ -7,15 +7,17 @@ import (
 	"path/filepath"
 )
 
-func JoinChunks(path, topic string) {
+func JoinChunks(path, title string) string {
 	config := GetConfig()
 
-	outfile := (filepath.Join(config.DownloadLocation, topic))
-	fmt.Println(outfile)
+	outfile := filepath.Join(config.DownloadLocation, title)
+	outfile = fmt.Sprintf("%s.mkv", outfile)
 
-	cmd := exec.Command("ffmpeg", "-y", "-hide_banner", "-allowed_extensions", "ts,m3u8", "-i", path, "-c", "copy", fmt.Sprintf("%s.mkv", outfile))
+	cmd := exec.Command("ffmpeg", "-y", "-hide_banner", "-allowed_extensions", "ts,m3u8", "-i", path, "-c", "copy", outfile)
 	if err := cmd.Run(); err != nil {
 		log.Fatal(err)
 	}
+
+	return outfile
 }
 
