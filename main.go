f80cb0ec6ba19812e11893ccabd6b10edbbf01df
pnicto <dharanikanth789@gmail.com>
2023-05-20 18:23:10 +0530

    feat: Piece everything together

--- main.go
+++ main.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"log"
+	"os/exec"
+)
+
 func main() {
+	_, err := exec.LookPath("ffmpeg")
+	if err != nil {
+		log.Fatalf("Please add ffmpeg to your path")
+	}
+
+	LoginAndSetToken()
+	courses := GetCourses()
+
+	courseIndex := ChooseCourse(courses)
+	lectures := GetLectures(courses[courseIndex])
+
+	startLectureIndex, endLectureIndex := ChooseLectures(lectures)
+	GetMetadata(lectures[startLectureIndex : endLectureIndex+1])
 }
