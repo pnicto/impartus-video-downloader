0017e1f2d09d77ce9beb180aa9d503226ef9e6b0
pnicto <dharanikanth789@gmail.com>
2023-05-24 11:01:40 +0530

    fix: Connection reset by peer when sending too many requests
    Idk how this works but this works ig?

--- http.go
+++ http.go
@@ -2,27 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
-
-var client *http.Client
 
 func GetClientAuthorized(url string, token string) *http.Response {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		fmt.Printf("Failed to create http request for GET %s", url)
+		log.Fatalf("Failed to create http request for GET %s", url)
 		panic(err)
 	}
 
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
 
-	if client != (&http.Client{}) {
-		client = &http.Client{}
-	}
+	client := &http.Client{}
 
 	response, err := client.Do(req)
 	if err != nil {
-		fmt.Printf("Request failed with error %v for GET %s", err, url)
+		log.Fatalf("Request failed with error %v for GET %s", err, url)
 	}
 
 	return response
