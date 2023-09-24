82a7f4eb4bf7420fec767d33e712589ebb39ea5c
Arpit Yadav <72170146+FaNnT0m@users.noreply.github.com>
2023-09-24 22:21:44 +0530

    Update ui.go
    Addressed the TODO comments with proper code.

--- ui.go
+++ ui.go
@@ -17,8 +17,20 @@ func ChooseCourse(courses Courses) int {
 	}
 	fmt.Println()
 	fmt.Println("Enter a number")
-	// TODO: Check input is within range and a number
-	fmt.Scanf("%d", &choice)
+
+	// Added the code to check the input whether it lies in the range or not
+	for {
+	    _, err := fmt.Scanf("%d", &choice)
+	    if err != nil {
+	        fmt.Println("Invalid input. Please enter a valid number.")
+	        continue
+	    }
+	    if choice < 1 || choice > len(courses) {
+	        fmt.Println("Invalid choice. Please enter a valid number within the range.")
+	        continue
+	    }
+	    break
+	}
 	log.Printf("User chose %d\n", choice)
 	log.Printf("Index is %d\n", choice-1)
 
@@ -40,10 +52,22 @@ func ChooseLectures(lectures Lectures) (int, int) {
 	}
 	fmt.Println()
 
-	// TODO: Add better range examples here
-	fmt.Println("Enter a range")
-	// TODO: Check input is within range and a number
-	fmt.Scanf("%d %d", &startIndex, &endIndex)
+	// Replaced fmt.Println("Enter a range") with below code adding a better example for the range
+	fmt.Println("Enter a range (e.g., 1 5 for lectures 1 through 5):")
+
+	// Again added the code to check if the input lies in the range
+	for {
+	    _, err := fmt.Scanf("%d %d", &startIndex, &endIndex)
+	    if err != nil {
+	        fmt.Println("Invalid input. Please enter two valid numbers separated by a space.")
+	        continue
+	    }
+	    if startIndex < 1 || endIndex < 1 || startIndex > len(lectures) || endIndex > len(lectures) || startIndex > endIndex {
+	        fmt.Println("Invalid range. Please enter a valid range within the lecture indices.")
+	        continue
+	    }
+	    break
+	}
 
 	log.Printf("User chose %d %d\n", startIndex, endIndex)
 	log.Printf("Indices are %d %d\n", startIndex-1, endIndex-1)
