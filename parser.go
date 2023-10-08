d25f18cb7fe20804e49a75a9a9e7d4a7a3a20c71
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2023-10-08 15:50:03 +0530

    feat(parser): add parser to get stream links
    instead of blindly using the same base url for every stream, with this new parser appropriate urls will be given

---
+++ parser.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+type ParsedPlaylist struct {
+	HasMultipleViews bool
+	KeyURL           string
+	FirstViewURLs    []string
+	SecondViewURLs   []string
+}
+
+func PlaylistParser(scanner *bufio.Scanner) ParsedPlaylist {
+	var parsedOutput ParsedPlaylist
+	var isFirstView = true
+	var firstViewUrls []string
+	var secondViewUrls []string
+
+	for scanner.Scan() {
+		l := scanner.Text()
+		if parsedOutput.KeyURL == "" && strings.HasPrefix(l, "#EXT-X-KEY") {
+			pattern := `URI="([^"]+)"`
+			re := regexp.MustCompile(pattern)
+			match := re.FindStringSubmatch(l)
+			if len(match) == 2 {
+				url := match[1]
+				parsedOutput.KeyURL = url
+			} else {
+				fmt.Println("URL not found in the line.")
+			}
+		} else if strings.HasPrefix(l, "#EXT-X-DISCONTINUITY") {
+			isFirstView = false
+		} else if !strings.HasPrefix(l, "#EXT") {
+			if isFirstView {
+				firstViewUrls = append(firstViewUrls, l)
+			} else {
+				secondViewUrls = append(secondViewUrls, l)
+			}
+		}
+	}
+
+	if isFirstView {
+		parsedOutput.HasMultipleViews = false
+		parsedOutput.FirstViewURLs = firstViewUrls
+		parsedOutput.SecondViewURLs = nil
+	} else {
+		parsedOutput.HasMultipleViews = true
+		parsedOutput.FirstViewURLs = firstViewUrls
+		parsedOutput.SecondViewURLs = secondViewUrls
+	}
+
+	return parsedOutput
+}
