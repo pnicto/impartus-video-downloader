6b18b7a20cd02fbb0c6cff27eb1d255fe4d7e7c9
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2023-10-16 19:00:34 +0530

    test: fix function call after refactoring

--- parser_test.go
+++ parser_test.go
@@ -1035,7 +1035,7 @@ func TestParser(t *testing.T) {
 			}()
 
 			scanner := bufio.NewScanner(f)
-			got := PlaylistParser(scanner, id, tc.filename)
+			got := PlaylistParser(scanner, id, tc.filename, 1)
 
 			if got.KeyURL != tc.expectedKeyURL {
 				t.Errorf("Got keyURL %s, expected %s", got.KeyURL, tc.expectedKeyURL)
@@ -1070,7 +1070,7 @@ func BenchmarkParser(b *testing.B) {
 			}()
 			scanner := bufio.NewScanner(f)
 			b.StartTimer()
-			_ = PlaylistParser(scanner, id, tc.filename)
+			_ = PlaylistParser(scanner, id, tc.filename, 2)
 			b.StopTimer()
 		})
 	}
