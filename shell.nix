f5cc8813170d8191f3ab5c6349a8bd66d96f3af2
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2025-02-18 22:10:51 +0530

    docs: fix typo

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    steam-run-free # for running on NixOS binaries on NixOS
+    steam-run-free # for running non NixOS binaries on NixOS
     ffmpeg_6-full
   ];
 }
